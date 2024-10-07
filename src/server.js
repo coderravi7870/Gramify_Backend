91cb0a5b6e7b48f591cf3c8aa3e3318334dba243
RaviRaj <rks829421@gmail.com>
2024-10-07 08:56:49 +0530

    bug fix

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://gramify-frontend.vercel.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
