c80e9540d7b48ee2e886b320d9522b6a8eb76231
RaviRaj <rks829421@gmail.com>
2024-10-07 09:16:01 +0530

    bug fix

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://friendly-frangollo-4e7c4b.netlify.app/",
     credentials: true,
   })
 );
